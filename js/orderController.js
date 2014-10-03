f9dbcfdbf97b0208169017bb98b17c11865470a4
Victor Fomenko <victorfomenko@me.com>
2014-10-03 16:52:10 +0400

    add global dynamic classes

--- js/orderController.js
+++ js/orderController.js
@@ -1,3 +1,20 @@
-app.controller("orderController", function(){
-    console.log("orderController");
+app.controller("orderController", function($scope){
+
+    //Default states
+    $scope.productSelected = 'PO';
+    $scope.productStates = [
+        { id: 'PO', name: 'Печать', class: "print-only", isActive: true },
+        { id: 'CP', name: 'На холсте', class: "canvas", isActive: false },
+        { id: 'FP', name: 'В раме', class: "frame", isActive: false }
+    ];
+    $scope.mainClass = $scope.productStates[0].class;
+
+
+    $scope.changeProduct = function(product){
+        $scope.productStates.forEach(function(item){
+            item.isActive = false
+        });
+        product.isActive = true;
+        $scope.mainClass = product.class;
+    }
 });
