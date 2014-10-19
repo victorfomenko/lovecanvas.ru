5e7ed51c6df84ef4f21e82cafb9ab361fae368d1
Victor Fomenko <victorfomenko@me.com>
2014-10-19 18:09:06 +0400

    feat: new SEO features

--- js/main.js
+++ js/main.js
@@ -1,6 +1,7 @@
 var app = angular.module('app', ['ui.router']);
 app.config(function($stateProvider, $urlRouterProvider, $locationProvider){
     $locationProvider.html5Mode(true);
+    $locationProvider.hashPrefix('!'); //Hashbang goes for compatibility with browsers that do not support html5 urls.
     $urlRouterProvider.otherwise("");
     $stateProvider
         .state('main', {
