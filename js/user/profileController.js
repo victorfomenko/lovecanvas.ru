76ec71f952caff0276c5903b063ba641cd731a0f
Victor Fomenko <victorfomenko@me.com>
2015-03-12 19:36:52 +0300

    added dropzone.js and dropzone dirrective

--- js/user/profileController.js
+++ js/user/profileController.js
@@ -3,6 +3,14 @@ app.controller('profileController', function ($scope, $http, $state, USER_ROLES,
     var userURL = $state.params.artistId;
     var requestUrl = '/ajax/ajax.php?act=artist&userurl=' + userURL;
     var result;
+    $scope.dropzoneConfig = {
+        parallelUploads: 1,
+        maxFilesize: 24,
+        dictDefaultMessage: "Изменить фото",
+        dictInvalidFileType: "Тип файла не поддерживается",
+        clickable: true,
+        uploadMultiple: false
+    };
     return $http.post(requestUrl)
         .success(function(data){
             if (typeof  data === 'object') {
